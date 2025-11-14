36e8052303c398659e4eecf770a1f271df60b119
Himanshu Prajapati <himanshuprajapati15072003@gmail.com>
2025-11-14 16:34:17 +0530

    cses counting tower problem finally this works on given time and space constraints.

--- csesProblems/countingTowers.cpp
+++ csesProblems/countingTowers.cpp
@@ -11,34 +11,35 @@ const int N=1e6+10;
 vector<vector<int>>dp(N,vector<int>(2));
 
 void solve(){
-int n;cin>>n;
 
-//dp[i][0] is the number of ways to fill the grid from ith row to n-1 th row such that an vertical block is 
-// trying to extend from i-1 th row 
+// dp[i][0]: Ways to build a tower of height i ending in two separate blocks ([ ][ ]).
 
-//dp[i][1] is the number of ways to fill the grid from ith row to n-1 th row such that an horizontal block 
-// is trying to extend from i-1 row 
+// dp[i][1]: Ways to build a tower of height i ending in one joined block ([ ]).
 
-//base case is dp[n][1]=1 and dp[n][0]=1 ;this is where our row end .
-// final subproblem dp[1][0]+dp[1][1];
-dp[n][0]=1;
-dp[n][1]=1;
+//base case is dp[1][1]=1 and dp[1][0]=1 ;this is where our row  .
+// final subproblem dp[n][0]+dp[n][1];
+dp[1][0]=1;
+dp[1][1]=1;
 
 
-for(int i=n-1;i>=0;i--){
-	dp[i][0]=(4ll*dp[i+1][0]+dp[i+1][1])%mod;
-	dp[i][1]=(2ll*dp[i+1][1]+dp[i+1][0])%mod;
+for(int i=2;i<N;i++){
+	dp[i][0]=(4ll*dp[i-1][0]+dp[i-1][1])%mod;
+	dp[i][1]=(2ll*dp[i-1][1]+dp[i-1][0])%mod;
 }
-cout<<(dp[1][0]+dp[1][1])%mod<<nl;	
+	
 }
 
 signed main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0); 
+    solve();
     int t=1;
     cin>>t;
     while(t--){
-        solve();
+    int n;cin>>n;
+     cout<<(dp[n][0]+dp[n][1])%mod<<nl;
     }
     return 0;
 }
+
+

9ade2488bc787fe750ddf75a8e28ea2fff299b29
Himanshu Prajapati <himanshuprajapati15072003@gmail.com>
2025-11-18 09:55:35 +0530

    problem based on inversion on concept divide and conquer

--- spoj/INVCNT_InversionCount.cpp
+++ spoj/INVCNT_InversionCount.cpp
@@ -8,69 +8,66 @@ int gcd(int a,int b){if(b==0)return a;return gcd(b,a%b);}
 const int inf=1e18;
 const int N=1e5+10;
 
-int merge_and_count(vector<int>& arr, vector<int>& temp, int left, int mid, int right) {
-    int i = left;    
-    int j = mid + 1; 
-    int k = left;    
-    int inversion_count = 0;
-    while ((i <= mid) && (j <= right)) {
-        if (arr[i] <= arr[j]) {
-            temp[k++] = arr[i++];
-        } else {
-            temp[k++] = arr[j++];
-            inversion_count += (mid - i + 1);
-        }
+vector<int>temp;
+vector<int>arr;
+int merge(int left,int mid,int right){
+int i=left;
+int j=mid+1;
+int k=left;
+int inversions=0;
+
+while(i<=mid && j<=right){
+	if(arr[i]<=arr[j]){
+		temp[k++]=arr[i++];
+		
+	}else{
+		temp[k++]=arr[j++];
+		inversions+=(mid-i+1);
+	}
+}
+while(i<=mid){
+	temp[k++]=arr[i++];
+}
+while(j<=right){
+	temp[k++]=arr[j++];
+}
+for(i=left;i<=right;i++){
+        arr[i]=temp[i];
     }
-    while (i <= mid) {
-        temp[k++] = arr[i++];
-    }
-    while (j <= right) {
-        temp[k++] = arr[j++];
-    }
-    for (i = left; i <= right; i++) {
-        arr[i] = temp[i];
-    }
-    return inversion_count;
+return inversions;
+}
+int divide(int left,int right){
+ // we are recursively dividing the array 
+	int inversions=0;
+	if(left<right){
+		int mid=left+(right-left)/2;
+		//left array 
+		inversions+=divide(left,mid);
+		//right array 
+		inversions+=divide(mid+1,right);
+		// merge the sort which was recursive.
+		inversions+=merge(left,mid,right);
+	}
+	return inversions;
 }
 
-int merge_sort_and_count(vector<int>& arr, vector<int>& temp, int left, int right) {
-    int inversion_count = 0;
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        inversion_count += merge_sort_and_count(arr, temp, left, mid);
-        inversion_count += merge_sort_and_count(arr, temp, mid + 1, right);
-        inversion_count += merge_and_count(arr, temp, left, mid, right);
-    }
-    return inversion_count;
-}
 void solve(){
-    int n;
-    cin >> n;
-    if (n == 0) return; // Handle edge case or end of input
-    
-    vector<int> A(n);
-    for (int i = 0; i < n; i++) {
-        cin >> A[i];
-    }
-    
-    vector<int> temp(n);
-    cout << merge_sort_and_count(A, temp, 0, n - 1) << nl;
+int n;cin>>n;
+arr.resize(n);
+temp.resize(n);
+for(auto &i:arr)cin>>i;
+cout<<divide(0,n-1) << nl;	
+
 }
 signed main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0); 
-    
-    int t;
-    cin >> t;
+    int t=1;
+    cin>>t;
     string blank;
-    getline(cin, blank); 
-    
+    getline(cin,blank);
     while(t--){
         solve();
-        if (t > 0) {
-            getline(cin, blank); 
-        }
     }
     return 0;
 }

ad6b9fa9637eac41fdf51c29ac39c2b782a802a9
Himanshu Prajapati <himanshuprajapati15072003@gmail.com>
2025-11-20 15:39:44 +0530

    this is party-party schedule question on spoj

--- spoj_dp_problems/KNAPSACK.cpp
+++ spoj_dp_problems/KNAPSACK.cpp
@@ -8,14 +8,24 @@ int gcd(int a,int b){if(b==0)return a;return gcd(b,a%b);}
 const int inf=1e18;
 const int N=1e5+10;
 
-void solve(){
-int S,n;cin>>S>>n;
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0); 
+    int t=1;
+    // cin>>t;
+    int S,n;
+    while(1){
+cin>>S>>n;
+if(S==0 && n==0)break;
 vector<int>size(n),values(n);
 for(int i=0;i<n;i++){
 	cin>>size[i]>>values[i];
 }
 //dp[i][j] is the maximum value achievalbe using weight i and total size not excedding S;
 vector<vector<int>>dp(S+1,vector<int>(n+1));
+int sum=0;
+int T=0;
 dp[0][0]=0;
 for(int s=0;s<=S;s++){
 	for(int j=1;j<=n;j++){
@@ -28,16 +38,21 @@ for(int s=0;s<=S;s++){
 		}
 	}
 }
-cout<<dp[S][n];
-}
+int max_fun = dp[S][n];
 
-signed main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0); 
-    int t=1;
-    // cin>>t;
-    while(t--){
-        solve();
+        int min_cost = 0;
+        
+        // Iterate through all possible budgets (costs) from the smallest (0) upwards.
+        for (int s = 0; s <= S; s++) {
+            if (dp[s][n] == max_fun) {
+                min_cost = s;
+                // Stop immediately upon finding the smallest cost (MinCost)
+                break; 
+            }
+        };
+
+        cout<<min_cost<<" ";
+cout<<dp[S][n]<<nl;
     }
     return 0;
 }
